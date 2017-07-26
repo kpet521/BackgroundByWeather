881848d889e5f37069b8ecb0276dead422fe01a8
Kayla Petrover <kpet521@umd.edu>
2017-07-26 13:12:47 -0400

    replaced the stuff

--- BackgroundByWeather.py
+++ BackgroundByWeather.py
@@ -1,65 +1,47 @@
-import urllib2
+from urllib2 import urlopen
 import json
-import re
 import ctypes
-import os
+import time
 
 #request weather data
 
 url = 'http://api.openweathermap.org/data/2.5/weather?zip=%s,us&APPID=%s'
 zip_code= '90024'
 API_KEY = 'c85d528ba3cdbcb4945277f6fb06f4de'
-response=urllib2.urlopen(url%(zip_code, API_KEY))
-json_string = response.read()
-    # if ^ is actually a string, why did I have to turn it into a string in 2 lines?
-parsed_json = json.loads(json_string)
-array = str(parsed_json.get('weather'))
-print array[15] #this gives you "e" woooo
 
-#how do I access the weather ID and not just the whole weather...
-#weather_ID = re.findall(r'2|3|4|5|6|7|8|9', weather)
-#print weather_ID
+    #loop every hour
+while API_KEY == 'c85d528ba3cdbcb4945277f6fb06f4de':
+    response=urlopen(url%(zip_code, API_KEY))
+    json_string = response.read()
+    parsed_json = json.loads(json_string)
+    ID= parsed_json.get('weather')[0].get('id')
 
 #map parsed response to a background image
 
-#if weather_ID == '8':
-    #image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\MiamiBeach2.jpg"
-    #SPI_SETDESKWALLPAPER = 20 
-    #ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, image_path, 3)
-#else:
-    #print('darn')
+    if ID < 233: #thunderstrom= 200-232
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\thunderstorm.jpg"
+    elif 299 < ID < 322: #drizzle= 300-321
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Drizzle.jpg"
+    elif  499 < ID < 532: #rain=500-531
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Rain.jpg"
+    elif 699 < ID < 623: #snow=600-622
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Snow.jpg"
+    elif 700 < ID < 782: #atm=701-781
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\ATM Italy.jpg"
+    elif ID == 800: #clear=800
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Clear landsEndForest.jpg"
+    elif 800 < ID < 805: #clouds=801-804
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Clouds Colosseum.jpg"
+    elif 899 < ID < 907: #extreme=900-906
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Xtreme landsendlabyrinth.jpg"
+    elif ID > 950: #wind=951-962
+        image_path = "C:\Users\kpet5\src\BackgroundByWeather\BackgroundPictures\Wind MiamiBeach2.jpg"
+#set background image
+    SPI_SETDESKWALLPAPER = 20 
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+    time.sleep(3600)
 
 
-#So the response you got back is formatted
-#as "json" which stands for JavaScript object
-#notation and is by far the most popular and
-#easiest way to serialize data
-#(data serialization is the process of taking
-#data and translating it into something that
-#can be sent to and understood by other programs)
-#Json is basically a big key / value mapping
-#The keys are all strings (strings are what
-#programmers call plain text) and the values
-#can either be strings, numbers, or more json
-#(another key / value mapping)
-#Specifically how you access the data in the json
-#response will depend on what you used to parse it,
-#but it will probably end up looking like
-#Response.get("weather").get("code")
-#So "weather" is a key and get("weather") returns
-#the value mapped to by the key, which in the example
-#is another json object
-#When I checked out that api I remember seeing in the
-#response definition some kind of weather code. I think
-#that would be the best thing to use to determine what picture to show
-
-#windows api to set the image
-    #drive = "C:\Users\kpet5\Pictures"
-    #folder = "Saved Pictures"
-    #image = "Colosseum.jpg"
-    #image_path = os.path.join(drive, folder, image)
-#for id=8XX
+#use startup to make it run in the background, while loop repeats
 
 
-
-#use startup to make it run in the background
--- weatherAPItest.py
+++ weatherAPItest.py
@@ -7,9 +7,8 @@ zip_code= '90024'
 API_KEY = 'c85d528ba3cdbcb4945277f6fb06f4de'
 response=urllib2.urlopen(url%(zip_code, API_KEY))
 json_string = response.read()
-    # if ^ is actually a string, why did I have to turn it into a string in 2 lines?
 parsed_json = json.loads(json_string)
-array = str(parsed_json.get('weather'))
-print array[15] #this gives you "e" woooo
+ID= parsed_json.get('weather')[0].get('id')
+print ID
 
 
